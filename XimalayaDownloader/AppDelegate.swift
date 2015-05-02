f41b8127063e33b10ff5c723893b69c798c22735
bindiry <bindiry@gmail.com>
2015-05-02 08:51:32 +0800

    Initial Commit

---
+++ XimalayaDownloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XimalayaDownloader
+//
+//  Created by bindiry on 5/2/15.
+//  Copyright (c) 2015 bindiry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XimalayaDownloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XimalayaDownloader
+//
+//  Created by bindiry on 5/2/15.
+//  Copyright (c) 2015 bindiry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
